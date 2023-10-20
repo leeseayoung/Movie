9ce9837f04f3291b9d49586c29673731b75eb3f4
Leeseayoung <tnfusrltk12@naver.com>
2023-10-20 13:06:11 +0900

    [이세영]
    -프로젝트 최초 생성 -

---
+++ src/main/java/com/penguin/movie/ex/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.penguin.movie.ex.hello;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@Controller
+public class HelloController {
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!!";
+    }
+}
